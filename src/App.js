33b46934d52ffe60872d32244b9c8230a5c8c212
Pobarul-islam <pobarul3@gmail.com>
2022-04-04 12:25:05 +0600

    product analysis website project started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import DashBoard from './Components/DashBoard/DashBoard';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import NotFound from './Components/NotFound/NotFound';
+import Reviews from './Components/Reviews/Reviews';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <Routes>
+        <Route path='/' element={<Header></Header>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+        <Route path='/dashBoard' element={<DashBoard></DashBoard>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        <h2>This is Header page</h2>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
